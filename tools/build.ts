6b52aa91eec5938fb66cc42dff928a62e4ed76e3
ericchase <github@eric.addymail.com>
2025-08-19 19:50:10 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -1,7 +1,8 @@
-import { BunPlatform_Args_Has } from '../src/lib/ericchase/BunPlatform_Args_Has.js';
+import { BunPlatform_Argv_Includes } from '../src/lib/ericchase/BunPlatform_Argv_Includes.js';
 import { Step_Dev_Format } from './core-dev/step/Step_Dev_Format.js';
 import { Step_Dev_Project_Update_Config } from './core-dev/step/Step_Dev_Project_Update_Config.js';
 import { Processor_HTML_Custom_Component_Processor } from './core-web/processor/Processor_HTML_Custom_Component_Processor.js';
+import { Processor_HTML_Remove_HotReload_On_Build } from './core-web/processor/Processor_HTML_Remove_HotReload_On_Build.js';
 import { Step_Run_Dev_Server } from './core-web/step/Step_Run_Dev_Server.js';
 import { Builder } from './core/Builder.js';
 import { Processor_Set_Writable } from './core/processor/Processor_Set_Writable.js';
@@ -11,7 +12,7 @@ import { Step_Dev_Generate_Links } from './lib-browser-userscript/steps/Step_Dev
 
 // await AddLoggerOutputDirectory('cache');
 
-if (BunPlatform_Args_Has('--dev')) {
+if (BunPlatform_Argv_Includes('--dev')) {
   Builder.SetMode(Builder.MODE.DEV);
 }
 Builder.SetVerbosity(Builder.VERBOSITY._1_LOG);
@@ -21,11 +22,11 @@ Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'update', '--latest'], showlogs: false }),
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
   Step_FS_Clean_Directory(Builder.Dir.Out),
-  Step_Dev_Format({ showlogs: false }),
   //
 );
 
 Builder.SetProcessorModules(
+  Processor_HTML_Remove_HotReload_On_Build(),
   Processor_HTML_Custom_Component_Processor(),
   Processor_Set_Writable({ include_patterns: ['**/*.user.css'], value: true }),
   //
@@ -34,6 +35,11 @@ Builder.SetProcessorModules(
 Builder.SetAfterProcessingSteps(
   Step_Dev_Generate_Links({ dirpath: Builder.Dir.Out, pattern: '**/*.user.css' }),
   Step_Run_Dev_Server(),
+  //
+);
+
+Builder.SetCleanUpSteps(
+  Step_Dev_Format({ showlogs: false }),
   //
 );
 
